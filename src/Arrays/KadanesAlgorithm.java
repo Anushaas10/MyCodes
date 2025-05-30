fbba3477cf2dada13d5d2791e1954f2b19dcd18c
Anushaas10 <singhanushaa10@gmail.com>
2025-05-30 20:53:03 +0530

    init commit

---
+++ src/Arrays/KadanesAlgorithm.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class KadanesAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, -8, 7, -1, 2, 3};
+        System.out.println(maxSubarraySum(arr));
+    }
+
+    static int maxSubarraySum(int[] arr) {
+        int sum = arr[0], max_sum=arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            sum = Math.max(sum + arr[i], arr[i]);
+            max_sum= Math.max(max_sum, sum);
+        }
+
+        return max_sum;
+    }
+}
