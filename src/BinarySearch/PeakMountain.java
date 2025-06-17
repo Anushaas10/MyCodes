49e272b077987d30ad7f0bcc824a8413f3ace34d
Anushaas10 <singhanushaa10@gmail.com>
2025-06-17 17:31:03 +0530

    init commit

--- src/BinarySearch/PeakMountain.java
+++ src/BinarySearch/PeakMountain.java
@@ -2,19 +2,22 @@ package BinarySearch;
 
 public class PeakMountain {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5, 6, 4, 3, 2};
+        int[] arr = {1, 2, 3};
         System.out.println(binarySearch(arr));
     }
 
     static int binarySearch(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
+        if (arr.length == 1) {
+            return 0;
+        }
         while (start < end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid + 1] > arr[mid]) {
-                start = mid + 1;
-            } else {
+            if (arr[mid] > arr[mid + 1]) {
                 end = mid;
+            } else {
+                start = mid + 1;
             }
         }
         return start;
