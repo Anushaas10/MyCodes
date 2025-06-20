e4d4c984398cb6e4764c850db44e0f0a732f02da
Anushaas10 <singhanushaa10@gmail.com>
2025-06-20 12:48:47 +0530

    init commit

--- src/Sorting/GFGhIndex.java
+++ src/Sorting/GFGhIndex.java
@@ -1,5 +1,8 @@
 package Sorting;
-
+//Counting sort is an algorithm for sorting a collection of objects according to keys that are small positive integers. It operates by counting the number of objects that have each distinct key value, and using arithmetic on those counts to determine the positions of each key.
+// The algorithm works by first counting the number of occurrences of each key, then using prefix sums to determine the positions where each key should be placed in the output array.
+// Counting sort is not a comparison sort; it uses key values as indexes into an array.
+//Counting sort is particularly efficient when the range of input values is small compared to the number of elements to be sorted.
 public class GFGhIndex {
     public static void main(String[] args) {
         int[] citations = {5, 1, 2, 4, 1};
